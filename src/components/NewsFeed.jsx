eab07dbb06760be60c0e38a04a6a23d89aaf971b
suyanthan <vsuya@hotmail.co.uk>
2018-05-14 00:32:47 +0100

    Updated NewsFeed component

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NewsCard from './NewsCard';
 import Filter from './Filter';
 class NewsFeed extends Component {
   	constructor(props) {
@@ -11,7 +12,29 @@ class NewsFeed extends Component {
 	    this.handleFilter = this.handleFilter.bind(this);
   	}
 
+  	componentWillMount() {
+    	this.fetchPosts();
+  	}
 
+  	fetchPosts(isSourceChanged=false) {
+  		let apiUrl;
+  		if(this.state.selectedSource === "all"){
+			apiUrl = `https://newsapi.org/v2/top-headlines?language=en&apiKey=f5002ab5bbfe40a39ade2a0d65fc3b73`
+  		}else{
+  			apiUrl = `https://newsapi.org/v2/top-headlines?sources=${this.state.selectedSource}&apiKey=f5002ab5bbfe40a39ade2a0d65fc3b73`
+  		}
+		fetch(`${apiUrl}&pageSize=5&page=${this.state.displayStory}`).then((response) => {
+      		return response.json();
+    	}).then((data) => {
+	      	if(isSourceChanged === false){
+		        let activeNewsFeed = this.state.newsFeed;
+		        activeNewsFeed.push.apply(activeNewsFeed,data.articles)
+		        this.setState({newsFeed:activeNewsFeed});
+	      	}else{
+	        	this.setState({newsFeed:data.articles});
+	     	}
+    	});
+	}
 
   	handleFilter(filter) {
 	    this.setState(() => ({pagination:1,selectedSource:filter}), () =>{
@@ -27,10 +50,15 @@ class NewsFeed extends Component {
 	}
 
   	render() {
+	    const posts = this.state.newsFeed.map((news, index) =>
+	      <NewsCard key={index} value={news} />
+	    );
+
 	    return (
 	      <div className="newsFeed">
 	        <Filter onFilter={this.handleFilter}/>
 	        <h1 className="newsFeed__title">News</h1>
+	        {posts}
 	        <button className="newsFeed__show-more" onClick={this.loadMoreNewsFeed}>Show more</button>
 	      </div>
 	    )
